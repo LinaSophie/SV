5408ed2932f6ce15d423118b0dce1dd60c6c387e
felsner <l.felsner@gmail.com>
2016-06-06 10:34:30 +0200

    dreiklang parameter

---
+++ dreiklang.py
@@ -0,0 +1,17 @@
+# erste Übuung Dreiklang
+
+
+
+# Parameter
+## Abtastwerte pro Sekunde
+rate = 16000
+
+## Frequenz für Sinusschwingung/Höhe des Sinustons
+f = 440
+
+## Länge des Signals in [sec]
+duration = 5
+
+print(float(f))
+print(type(float(f)))
+
